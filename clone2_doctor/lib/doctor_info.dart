51e38a95aefa2f33b8f045a506a597d9ac8c5b2c
sky688372-wq <sky688372@gmail.com>
2026-08-04 10:41:31 +0900

    Delete clone2_doctor directory

--- clone2_doctor/lib/doctor_info.dart
+++
@@ -1,8 +0,0 @@
-class DoctorInfo {
-  String name;
-  String introduce;
-  String ProfilePath;
-  double rating;
-
-  DoctorInfo({required this.name, required this.introduce, required this.ProfilePath, required this.rating});
-}
